249a3640ff3190191fe4614f9462d1525014f65f
Thomas Morgner <tmorgner@rabbitstewdio.com>
2019-02-03 15:40:20 +0000

    Updating version info for staging branch 3.0.0-beta.1

--- src/AutoCake.Unity/Properties/AssemblyInfo.cs
+++ src/AutoCake.Unity/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -11,6 +11,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
+
+[assembly: AssemblyInformationalVersion("3.0.0-beta.1+34.Branch.release-2.0.0.Sha.b4759ede2f4e44056105ab0f6c1022c7fd583321")]
